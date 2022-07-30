93380932b3dcef9b9981715454aab7e973bfc1cb
Hanjun Dai <daihanjun@gmail.com>
2022-07-30 18:33:37 +0000

    sync

--- test.py
+++ test.py
@@ -248,52 +248,14 @@ def gen_image(label, FLAGS, model, im_neg, num_steps, sample=False):
     for i in range(num_steps):
         im_noise.normal_()
 
-        if FLAGS.anneal:
-            im_neg = im_neg + 0.001 * (num_steps - i - 1) / num_steps * im_noise
-        else:
-            im_neg = im_neg + 0.001 * im_noise
+        im_neg = im_neg + 0.001 * im_noise
 
         im_neg.requires_grad_(requires_grad=True)
         energy = model.forward(im_neg, label)
 
-        if FLAGS.all_step:
-            im_grad = torch.autograd.grad([energy.sum()], [im_neg], create_graph=True)[0]
-        else:
-            im_grad = torch.autograd.grad([energy.sum()], [im_neg])[0]
+        im_grad = torch.autograd.grad([energy.sum()], [im_neg])[0]
 
-        if i == num_steps - 1:
-            im_neg_orig = im_neg
-            im_neg = im_neg - FLAGS.step_lr * im_grad
-
-            if FLAGS.dataset == "cifar10":
-                n = 128
-            elif FLAGS.dataset == "celeba":
-                # Save space
-                n = 128
-            elif FLAGS.dataset == "lsun":
-                # Save space
-                n = 32
-            elif FLAGS.dataset == "object":
-                # Save space
-                n = 32
-            elif FLAGS.dataset == "mnist":
-                n = 32
-            elif FLAGS.dataset == "imagenet":
-                n = 32
-            elif FLAGS.dataset == "stl":
-                n = 32
-
-            im_neg_kl = im_neg_orig[:n]
-            if sample:
-                pass
-            else:
-                energy = model.forward(im_neg_kl, label)
-                im_grad = torch.autograd.grad([energy.sum()], [im_neg_kl], create_graph=True)[0]
-
-            im_neg_kl = im_neg_kl - FLAGS.step_lr * im_grad[:n]
-            im_neg_kl = torch.clamp(im_neg_kl, 0, 1)
-        else:
-            im_neg = im_neg - FLAGS.step_lr * im_grad
+        im_neg = im_neg - FLAGS.step_lr * im_grad
 
         im_neg = im_neg.detach()
 
@@ -303,9 +265,9 @@ def gen_image(label, FLAGS, model, im_neg, num_steps, sample=False):
         im_neg = torch.clamp(im_neg, 0, 1)
 
     if sample:
-        return im_neg, im_negs_samples, im_grad
+        return im_neg, im_negs_samples
     else:
-        return im_neg, im_grad
+        return im_neg
 
 
 def test(model, logger, dataloader):
@@ -369,7 +331,7 @@ def train(models, models_ema, optimizer, logger, dataloader, resume_iter, logdir
             ix = random.randint(0, len(models) - 1)
             model = models[ix]
 
-            im_neg, im_samples, x_grad = gen_image(label, FLAGS, model, data_corrupt, num_steps, sample=True)
+            im_neg, im_samples = gen_image(label, FLAGS, model, data_corrupt, num_steps, sample=True)
 
             energy_pos = model.forward(data, label[:data.size(0)])
             energy_neg = model.forward(im_neg.clone(), label)
